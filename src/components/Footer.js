a1a15172d664950892a9e94661fcb6a911d58daa
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-25 21:33:23 -0500

    set up footer w/ links to github and linkedin

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <footer className='footer'>
+            <div>
+                <a href="https://github.com/DejahHarris" target="-blank" rel="noopener noreferrer" alt="GitHub" ></a>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/dejah-harris-858633259/" target="-blank" rel="noopener noreferrer" alt="LinkedIn" ></a>
+            </div>
+        </footer>
+    );
+}
