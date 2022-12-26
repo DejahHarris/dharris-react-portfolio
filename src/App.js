574f3f81405c08585c20c25e7e0e55f143e18093
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-25 21:56:41 -0500

    set up app

--- src/App.js
+++ src/App.js
@@ -8,5 +8,33 @@ import Contact from "./components/Contact";
 import Resume from "./components/Resume";
 
 export default function App() {
-  
+  const [currentTab, setCurrentTab] = useState("about");
+  const renderTab = () => {
+		switch (currentTab) {
+			case "about":
+				return <About />;
+        case "projects":
+          return <Projects />;
+          case "contact":
+				return <Contact />;
+			case "resume":
+				return <Resume />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div>
+			<div className="mobile-header">
+				<Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
+			</div>
+			<div>
+				<main>{renderTab()}</main>
+			</div>
+			<div>
+				<Footer></Footer>
+			</div>
+		</div>
+	);
 }
