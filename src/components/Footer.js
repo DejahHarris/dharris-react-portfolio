8c9b67479e748bb6aa9ca48239910d04ec223d61
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-25 21:35:57 -0500

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ export default function Footer() {
     return (
         <footer className='footer'>
             <div>
-                <a href="https://github.com/DejahHarris" target="-blank" rel="noopener noreferrer" alt="GitHub" ></a>
+                <a href="https://github.com/DejahHarris" target="-blank" rel="noopener noreferrer" alt="GitHub">GitHub</a>
             </div>
             <div>
-                <a href="https://www.linkedin.com/in/dejah-harris-858633259/" target="-blank" rel="noopener noreferrer" alt="LinkedIn" ></a>
+                <a href="https://www.linkedin.com/in/dejah-harris-858633259/" target="-blank" rel="noopener noreferrer" alt="LinkedIn">LinkedIn</a>
             </div>
         </footer>
     );
