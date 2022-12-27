f9eefac623efbaf98ba9da0365a05450f40e260f
Dejah Harris <dejahharris@Dejahs-MBP.attlocal.net>
2022-12-26 19:36:58 -0500

    fixed react errors- except for bootstrap card-not sure whats wrong

--- src/components/Projectsdata.js
+++ src/components/Projectsdata.js
@@ -1,19 +1,19 @@
 import React, {useState} from "react";
-import Card from "react-bootstrap/Card";
+import Card from "react"
 
 export default function Projectsdata (props) {
     const currentProject = useState(props)[0].projects;
     const name = currentProject.name;
     const description = currentProject.description;
     const image = currentProject.image;
-    const applink = currentProject.deployed;
-    const gitlink = currentProject.github;
+    const appLink = currentProject.deployed;
+    const gitLink = currentProject.github;
 
     return (
         <Card style={{ width: "18rem" }}>
 			<Card.Img
 				variant="top"
-				src={require(`../../assets/images/${image}`)}
+				src={require(`../assets/images${image}`)}
 				className="card-image"
 			/>
 			<div className="center">
